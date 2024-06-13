06f4510b3d18f4ab34e77506e0666df7a666f90c
burakcinardeu <124098194+burakcinardeu@users.noreply.github.com>
2024-06-13 09:56:36 +0200

    Create tests.sh

---
+++ project/tests.sh
@@ -0,0 +1,23 @@
+import os
+import unittest
+import sqlalchemy as sql
+
+class TestPipeline(unittest.TestCase):
+
+    def test_sqlite_db_exists(self):
+        db_path = '../data/data.sqlite'
+        self.assertTrue(os.path.exists(db_path), "SQLite database does not exist")
+
+    def test_emissions_table_exists(self):
+        engine = sql.create_engine('sqlite:///../data/data.sqlite')
+        inspector = sql.inspect(engine)
+        self.assertIn('emissions', inspector.get_table_names(), "Emissions table does not exist in the database")
+
+    def test_temperature_table_exists(self):
+        engine = sql.create_engine('sqlite:///../data/data.sqlite')
+        inspector = sql.inspect(engine)
+        self.assertIn('temperature', inspector.get_table_names(), "Temperature table does not exist in the database")
+
+if __name__ == '__main__':
+    unittest.main()
+
